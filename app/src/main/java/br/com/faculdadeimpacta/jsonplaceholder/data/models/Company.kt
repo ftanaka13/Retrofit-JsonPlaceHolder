808209f266ca710e7c5eb1aef8431727c3afe4be
Fernando Tanaka <fernando.tanaka@outlook.com>
2024-09-19 20:55:36 -0300

    Data class do User criada

---
+++ app/src/main/java/br/com/faculdadeimpacta/jsonplaceholder/data/models/Company.kt
@@ -0,0 +1,7 @@
+package br.com.faculdadeimpacta.jsonplaceholder.data.models
+
+data class Company(
+    val bs: String,
+    val catchPhrase: String,
+    val name: String
+)
---
+++ app/src/main/java/br/com/faculdadeimpacta/jsonplaceholder/data/models/Geo.kt
@@ -0,0 +1,6 @@
+package br.com.faculdadeimpacta.jsonplaceholder.data.models
+
+data class Geo(
+    val lat: String,
+    val lng: String
+)
